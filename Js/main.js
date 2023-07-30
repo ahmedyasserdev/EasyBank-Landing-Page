4ee2ac26e78a05c28510e2d6c93e6ae659f12fd0
ahmedyasserdev <ahmedyasser3033@gmail.com>
2023-07-30 17:20:12 +0300

    first commit

---
+++ Js/main.js
@@ -0,0 +1,8 @@
+let togglerIcon = document.querySelector(".fa-bars")
+let togglerBtn = document.querySelector(".toggler")
+
+togglerBtn.addEventListener("click" , () => {
+    togglerIcon.classList.toggle("fa-times")
+} )
+
+
